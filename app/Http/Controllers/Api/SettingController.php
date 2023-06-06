fbf008b6ba2bbfa423685920bb283f66c63781ed
mahmoud-saber <60474033+mahmoud-saber@users.noreply.github.com>
2023-06-06 03:27:27 +0300

    create Setting Resource

--- app/Http/Controllers/Api/SettingController.php
+++ app/Http/Controllers/Api/SettingController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Models\Setting;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\SettingResource;
 
 class SettingController extends Controller
 {
@@ -13,7 +14,7 @@ class SettingController extends Controller
      */
     public function __invoke(Request $request)
     {
-        $settings=Setting::all();
-        return $settings;
+        $settings=Setting::findOrFail(1);
+        return  new SettingResource($settings);
       }
 }
