59e532daff9b66e5dbae43ca86037b5f36cdbaf7
mahmoud-saber <60474033+mahmoud-saber@users.noreply.github.com>
2023-06-07 03:38:14 +0300

    ApiResponse schema

--- app/Http/Controllers/Api/SettingController.php
+++ app/Http/Controllers/Api/SettingController.php
@@ -2,7 +2,8 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Models\Setting;
+ use App\Models\Setting;
+use App\Helpers\ApiResponse;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\SettingResource;
@@ -17,7 +18,15 @@ class SettingController extends Controller
         // $settings=Setting::findOrFail(1);
         // return  new SettingResource($settings);
 
-        $settings=Setting::get();
-        return SettingResource::collection($settings);
+        // $settings=Setting::get();
+        // return SettingResource::collection($settings);
+
+        // $settings = Setting::find(1);
+        $settings = Setting::find(3);
+        if($settings){
+            return ApiResponse::SendResponse(200,'Settings Retrieved successful',new SettingResource($settings));
+        }
+            return ApiResponse::SendResponse(200,'Settings not found','[]');
+
       }
 }
