ed4b22570cded73f6a54de042493387908ffd78a
mahmoud-saber <60474033+mahmoud-saber@users.noreply.github.com>
2023-06-09 22:05:53 +0300

    Advertisement module Customize pagintion links

--- app/Http/Controllers/Api/AdvertismentController.php
+++ app/Http/Controllers/Api/AdvertismentController.php
@@ -13,7 +13,23 @@ class AdvertismentController extends Controller
     public function index(){
         $advent=Advertisement::latest()->paginate(1);
         if(count($advent) > 0){
-            return ApiResponse::SendResponse(200,'Advertisment Retrieved Successful',AdvertismentResource::collection($advent));
+            if($advent->total() > $advent->perPage()){
+                $data =[
+                    'recored'=>AdvertismentResource::collection($advent),
+                    'pagination Links'=>[
+                        'current page'=>$advent->currentPage(),
+                        'per_page'=>$advent->perPage(),
+                        'total_page'=>$advent->total(),
+                        'links'=>[
+                            'first'=>$advent->url(1),
+                            'last'=>$advent->url($advent->lastPage()),
+                        ],
+                    ],
+                ];
+            }else{
+                $data=AdvertismentResource::collection($advent);
+            }
+            return ApiResponse::SendResponse(200,'Advertisment Retrieved Successful',$data);
         }
         return ApiResponse::SendResponse(200,'No Advertisment Retrieved ',[]);
 
